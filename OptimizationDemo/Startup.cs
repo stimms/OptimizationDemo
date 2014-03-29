b7cea9ce451ad5580910826a28096b5be7126c1c
Simon Timms <stimms@gmail.com>
2014-03-29 11:24:07 -0600

    Initial add. Next up - make things terrible

---
+++ OptimizationDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OptimizationDemo.Startup))]
+namespace OptimizationDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
