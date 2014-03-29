0297751dc9cf2aeda5844e3d8adabcbf2e716193
Simon Timms <stimms@gmail.com>
2014-03-29 14:30:47 -0600

    Adding some poorly loading images

--- OptimizationDemo/App_Start/BundleConfig.cs
+++ OptimizationDemo/App_Start/BundleConfig.cs
@@ -18,7 +18,7 @@ namespace OptimizationDemo
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
-
+    
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
